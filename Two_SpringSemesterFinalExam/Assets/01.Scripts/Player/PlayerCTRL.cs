c15100fbf8c1223f13bc833bee51ae52d3d3b990
parechae123 <parechae@gmail.com>
2023-04-25 04:35:13 +0900

    플레이어 점프,더블점프 애니메이션 수정및 플레이어 이단점프 구현

--- Two_SpringSemesterFinalExam/Assets/01.Scripts/Player/PlayerCTRL.cs
+++ Two_SpringSemesterFinalExam/Assets/01.Scripts/Player/PlayerCTRL.cs
@@ -8,16 +8,20 @@ public class PlayerCTRL : MonoBehaviour
     private Rigidbody2D rb;
     private float playerMoveAxis;
     private SpriteRenderer sr;
-    [SerializeField] public PlayerStats playerStats;
+    public LayerMask whatIsGround;
+    private CapsuleCollider2D cc;
+    public byte jumpCount = 0;
+    [SerializeField] public PlayerStats pS;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        cc = GetComponent<CapsuleCollider2D>();
     }
     private void Update()
     {
-        rb.velocity = new Vector2((playerMoveAxis*playerStats.moveSpeed)*Time.deltaTime, rb.velocity.y);
+        rb.velocity = new Vector2((playerMoveAxis*pS.moveSpeed)*Time.deltaTime, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
@@ -34,15 +38,35 @@ public class PlayerCTRL : MonoBehaviour
     }
     public void OnJump(InputAction.CallbackContext ctx)
     {
-        if (ctx.started)
+        if (ctx.started&&jumpCount<2)
         {
-            rb.velocity+= Vector2.up * playerStats.jumpForce;
+            rb.velocity+= Vector2.up * pS.jumpForce;
+            jumpCount++;
+            Debug.Log("jumped");
+            if(jumpCount == 1)
+            {
+                StartCoroutine(jumpSencer());
+            }
         }
+
     }
     [System.Serializable] public struct PlayerStats
     {
         public float moveSpeed;
         public float playerHP;
         public float jumpForce;
+    }
+    private IEnumerator jumpSencer()
+    {
+        yield return new WaitUntil(() => rb.velocity.y < 0.5f);
+        while (jumpCount > 0)
+        {
+            yield return new WaitForEndOfFrame();
+            if (Physics2D.Raycast(transform.position, Vector2.down, cc.bounds.extents.y+0.01f, whatIsGround))
+            {
+                Debug.DrawRay(transform.position, Vector3.down * cc.bounds.extents.y, Color.red);
+                jumpCount = 0;
+            }
+        }
     }
 }
